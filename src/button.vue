1db13de3efb47e80923c25da3fb9eca684d72421
jgoriginal <932274204@qq.com>
2022-01-20 21:36:17 +0800

    创建button-group

--- src/button.vue
+++ src/button.vue
@@ -30,7 +30,7 @@ export default {
 <style lang="scss">
 .g-button {
   font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);background: var(--button-bg);display: inline-flex;justify-content: center;
+  border: 1px solid var(--border-color);background: var(--button-bg);display:inline-flex;justify-content: center;
   align-items: center;vertical-align: middle;
   &:hover {border-color: var(--border-color-hover);}
   &:active {background-color: var(--button-active-bg);}
