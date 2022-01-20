e571bc5021ba68d2d77fb08bd4e4c262f691295e
jgoriginal <932274204@qq.com>
2022-01-20 16:02:37 +0800

    button组件支支持icon和iconPositionè属性

--- src/button.vue
+++ src/button.vue
@@ -1,28 +1,30 @@
 <template>
-  <div>
-    <button class="g-button">按钮</button>
-  </div>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+      <svg v-if="icon" class="icon" aria-hidden="true">
+        <use :xlink:href="`#icon-${icon}`"></use>
+      </svg>
+      <div class="content">
+        <slot/>
+      </div>
+    </button>
 </template>
 <script>
 export default {
+  props: ['icon', 'iconPosition']
 }
 </script>
 <style lang="scss">
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
-  padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
-  &:hover {
-    border-color: var(--border-color-hover);
-  }
-  &:active {
-    background-color: var(--button-active-bg);
-  }
-  &:focus {
-    outline: none;
-  }
+  font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);background: var(--button-bg);display: inline-flex;justify-content: center;
+  align-items: center;vertical-align: middle;
+  &:hover {border-color: var(--border-color-hover);}
+  &:active {background-color: var(--button-active-bg);}
+  &:focus {outline: none;}
+  > .icon {width: 1em;height: 1em;order: 1;margin-right: 0.5em;margin-left: 0;}
+  > .content {order: 2}
+  &.icon-right {
+    > .icon {order: 2;margin-right: 0em;margin-left: 0.5em;}
+    > .content {order: 1;} }
 }
 </style>
